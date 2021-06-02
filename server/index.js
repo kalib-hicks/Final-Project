31c0a18838ee67de4f1c067e0a9cbd8576db7e3d
Kalib Hicks <kalib.hicks@gmail.com>
2021-06-02 14:44:44 -0400

    added mongodb connection

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser:true, useUnifiedTopology:true
 }).then(() => app.listen(PORT, () =>
-console.log(`Connection is established and running on port: ${PORT}`)
+    console.log(`Connection is established and running on port: ${PORT}`)
 )).catch((err) => console.log(err.message));
 
 mongoose.set('useFindAndModify',false);
