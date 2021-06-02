ac63493d0e11a33cefea9fdd0015e1760eee206d
Kalib Hicks <kalib.hicks@gmail.com>
2021-06-02 14:39:31 -0400

    added mongodb connection

--- server/index.js
+++ server/index.js
@@ -3,3 +3,21 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 const app = express();
+
+app.use(express.json({limit: "20mb", extended:true});
+app.use(express.urlencoded({limit: "20mb", extended:true}));
+
+app.use(cors());
+
+
+const CONNECTION_URL = "mongodb+srv://kalib-hicks:Ktkh.1218958!?.@cluster0.sspdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser:true, useUnifiedTopology:true
+}).then() => app.listen(PORT, () =>
+console.log(`Connection is established and running on port: ${PORT}`)
+)).catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify',false);
