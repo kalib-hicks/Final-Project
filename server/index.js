45f10a34b9f36e6a66e346aee743720109b96329
Kalib Hicks <kalib.hicks@gmail.com>
2021-06-02 15:19:46 -0400

    added mongodb connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
 const app = express();
 
 app.use(express.json({limit: "20mb", extended:true}));
@@ -10,7 +11,7 @@ app.use(express.urlencoded({limit: "20mb", extended:true}));
 app.use(cors());
 
 
-const CONNECTION_URL = 'mongodb+srv://kalib-hicks:Bard1218958@cluster0.sspdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://kalib-hicks:Bard1218958@cluster0.sspdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
 
